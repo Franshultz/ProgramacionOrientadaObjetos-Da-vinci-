5dccb5822fe7dbfa8bdd8d74b08b8810cc9d48f8
Franco Aparicio <francomaty07@gmail.com>
2024-06-04 15:40:28 -0300

    Create folder

--- clase14/Main.java
+++ clase14/Main.java
@@ -1,15 +1,10 @@
 import javax.swing.JOptionPane;
 
-public class Main {
+public class main {
 
 	public static void main(String[] args) {
 		
-		 int numero = 0;
-		 if (numero = 0) {
-			JOptionPane.showMessageDialog(null, "El numero es igual a 0");
-		} else {
-			JOptionPane.showMessageDialog(null, "El numero no es igual a 0");
-		}
+		JOptionPane.showMessageDialog(null, "CLASE14!!!");
 
 	}
 
