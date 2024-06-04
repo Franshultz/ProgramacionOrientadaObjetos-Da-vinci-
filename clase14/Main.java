86c9d2ba297ed4d38542814853b2166fc3ec2625
Franco Aparicio <francomaty07@gmail.com>
2024-06-04 15:22:35 -0300

    agreggo el main y la class Nota

---
+++ clase14/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		 int numero = 0;
+		 if (numero = 0) {
+			JOptionPane.showMessageDialog(null, "El numero es igual a 0");
+		} else {
+			JOptionPane.showMessageDialog(null, "El numero no es igual a 0");
+		}
+
+	}
+
+}
