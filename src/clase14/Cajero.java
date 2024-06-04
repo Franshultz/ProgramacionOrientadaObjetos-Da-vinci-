ced5c21dd178c86998ca512628b43faeddc1002b
Franco Aparicio <francomaty07@gmail.com>
2024-06-04 20:23:06 -0300

    agrego clases de cajero y cuenta

---
+++ src/clase14/Cajero.java
@@ -0,0 +1,39 @@
+package clase14;
+
+public class Cajero {
+	
+	private  String ubicaion;
+	private  double saldo;
+	
+	public Cajero(String ubicaion, double saldo) {		
+		this.ubicaion = ubicaion;
+		this.saldo = saldo;
+	}
+
+	
+	public String getUbicaion() {
+		return ubicaion;
+	}
+
+	public void setUbicaion(String ubicaion) {
+		this.ubicaion = ubicaion;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Cajero [ubicaion=" + ubicaion + ", saldo=" + saldo + "]";
+	}
+	
+	
+	
+	
+}
---
+++ src/clase14/Cuenta.java
@@ -0,0 +1,62 @@
+package clase14;
+
+public class Cuenta {
+	
+	
+	private  int nroCliente;
+	private  double saldo;
+	private  Cliente cliente;
+	private  String operaciones;
+	
+	public Cuenta(int nroCliente, double saldo, Cliente cliente, String operaciones) {
+		this.nroCliente = nroCliente;
+		this.saldo = saldo;
+		this.cliente = cliente;
+		this.operaciones = operaciones;
+	}
+
+	public int getNroCliente() {
+		return nroCliente;
+	}
+
+	public void setNroCliente(int nroCliente) {
+		this.nroCliente = nroCliente;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public String getOperaciones() {
+		return operaciones;
+	}
+
+	public void setOperaciones(String operaciones) {
+		this.operaciones = operaciones;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Cuenta [nroCliente=" + nroCliente + ", saldo=" + saldo + ", cliente=" + cliente + ", operaciones="
+				+ operaciones + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
