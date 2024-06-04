b09740e0359a0df12bbd4186e8337530702e7231
Franco Aparicio <francomaty07@gmail.com>
2024-06-04 20:16:55 -0300

    agrego constructor a cliente

--- src/clase14/Cliente.java
+++ src/clase14/Cliente.java
@@ -1,5 +1,13 @@
 package clase14;
 
-public class Cliente {
+public class Cliente extends Usuario{
 
+	public Cliente(String nombre, String dni, String pin, String rol) {
+		super(nombre, dni, pin, rol);
+	}
+	
+	
+
+	
+	
 }
--- src/clase14/Empleado.java
+++ src/clase14/Empleado.java
@@ -1,5 +1,44 @@
 package clase14;
 
-public class Empleado {
+public class Empleado extends Usuario{
+	
+	private  String puesto;
+	private  String departamento;
+	private  int sueldo;
+	
+	public Empleado(String nombre, String dni, String pin, String rol, String puesto, String departamento, int sueldo) {
+		super(nombre, dni, pin, rol);
+		this.puesto = puesto;
+		this.departamento = departamento;
+		this.sueldo = sueldo;
+	}
 
+	
+	public String getPuesto() {
+		return puesto;
+	}
+
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public int getSueldo() {
+		return sueldo;
+	}
+
+	public void setSueldo(int sueldo) {
+		this.sueldo = sueldo;
+	}
+	
+	
+	
+	
 }
