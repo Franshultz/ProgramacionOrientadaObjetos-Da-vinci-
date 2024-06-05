149e1bd2c9033ec5323b577bb4e9d39d4a53694f
Franshultz <francomaty07@gmail.com>
2024-06-05 13:00:27 -0300

    Agrego metodos agregar y eliminar jugadores

--- src/Equipo.java
+++ src/Equipo.java
@@ -1,15 +1,17 @@
 import java.util.LinkedList;
 
+import javax.swing.JOptionPane;
+
 public class Equipo {
 	
 	private  String nombreClub;
 	private  String ciudad;
-	private  LinkedList Jugadores = null;
+	private LinkedList<Jugador> jugadores;
 	
 	public Equipo(String nombreClub, String ciudad) {
 		this.nombreClub = nombreClub;
 		this.ciudad = ciudad;
-		this.Jugadores = Jugadores;
+		this.jugadores = new LinkedList<>();
 	}
 
 	public String getNombreClub() {
@@ -28,26 +30,74 @@ public class Equipo {
 		this.ciudad = ciudad;
 	}
 
+
 	public LinkedList getJugadores() {
-		return Jugadores;
+		return jugadores;
 	}
 
 	public void setJugadores(LinkedList jugadores) {
-		Jugadores = jugadores;
+		jugadores = jugadores;
 	}
 
 	@Override
 	public String toString() {
-		return "Equipo [nombreClub=" + nombreClub + ", ciudad=" + ciudad + ", Jugadores=" + Jugadores + "]";
+		return "Equipo [nombreClub=" + nombreClub + ", ciudad=" + ciudad + ", Jugadores=" + jugadores + "]";
 	}
+
 	
-	public void AgregarListaJugadores(LinkedList<?> ListaJugadores) {
-		
+	//AGREGO EQUIPO ENTERO
+	public void AgregarListaJugadores(LinkedList ListaJugadores) {
+		this.setJugadores(ListaJugadores);
 	} 
 	
+	//AGREGO JUGADOR ESPECIFICO
+	public boolean AgregarJugador() {
+		 String nombre = JOptionPane.showInputDialog("Ingrese nombre del jugador nuevo");
+		 String posicion = JOptionPane.showInputDialog("Ingrese posicion del jugador nuevo");
+		 String numeroCamiseta = JOptionPane.showInputDialog("Ingrese el numero de camiseta del jugador nuevo");
+		 String edadJugador = JOptionPane.showInputDialog("Ingrese la edad del jugador nuevo");
+		 Jugador jugadorNuevo = new Jugador(nombre, posicion, numeroCamiseta, edadJugador);
+		    
+		 LinkedList<Jugador> ListaJugadores = getJugadores();	    
+		 if (ListaJugadores.add(jugadorNuevo)) {
+			 setJugadores(ListaJugadores);
+			 return true;
+		} else {
+			return false;
+		}
+	}
 	
+	public boolean EliminarJugador() {
+        String[] nombresJugadoresEliminar = new String[jugadores.size()];
+        int i = 0;
+        for (Jugador jugador : jugadores) {
+            nombresJugadoresEliminar[i] = jugador.getNombre();
+            i++;
+        }
+        
+        
+        String seleccionJugadorEliminar = (String)JOptionPane.showInputDialog(null, "Seleccione una opción:", "Menú", JOptionPane.QUESTION_MESSAGE, null, nombresJugadoresEliminar, nombresJugadoresEliminar[0]);
+        if (seleccionJugadorEliminar != null) {
+        	for (int index = 0; index < jugadores.size(); index++) {
+        		if (jugadores.get(index).getNombre().equals(seleccionJugadorEliminar)) {
+        			jugadores.remove(index);
+        		}
+        	}	
+        	return true;
+		} else {
+			return false;
+		}
+	}
 	
+	public boolean CantidadJugadores() {
+		
+		return true;
+	}     
 	
+	public LinkedList ObtenerListaJugadores() {
+		
+		return jugadores;
+	}
 	
 	
 }
--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,76 @@ import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 4; i++) {
+		
+		//CREO EQUIPOS Y JUGADORES
+		Equipo[] equipo = new Equipo[2];
+		for (int i = 0; i < 2; i++) {
 			LinkedList<Jugador> ListaJugadores = new LinkedList<Jugador>();
 			String nombreClub = JOptionPane.showInputDialog("Ingrese nombre del Club: ");
 			String ciudad = JOptionPane.showInputDialog("Ingrese nombre de la Ciudad del club: ");
 			
-			Equipo equipo = new Equipo(nombreClub, ciudad);
-			for (int index= 0; index < 11; index++) {
+			equipo[i] = new Equipo(nombreClub, ciudad);
+			Jugador[] jugador = new Jugador[2];
+			for (int index= 0; index < 2; index++) {
 				String nombre = JOptionPane.showInputDialog("Ingrese nombre de Jugador: ");
 				String posicion = JOptionPane.showInputDialog("Ingrese nombre de Poscicion: ");
 				String numeroCamiseta = JOptionPane.showInputDialog("Ingrese Numero de camiseta: ");
 				String edadJugador = JOptionPane.showInputDialog("Ingrese Edad de jugador: ");
 			
-				Jugador jugador = new Jugador(nombre, posicion, numeroCamiseta, edadJugador);
-				ListaJugadores.add(jugador);
+				jugador[i] = new Jugador(nombre, posicion, numeroCamiseta, edadJugador);
+				ListaJugadores.add(jugador[i]);
 		    }
-			equipo.AgregarListaJugadores(ListaJugadores);
-
+			equipo[i].AgregarListaJugadores(ListaJugadores);
 	    }
+		
+		int eleccionEquipo = 0;
+		int eleccionOperacion = 0;
+		do {
+			
+			String[] opcionesEquipo = new String[2];
+	        for (int i = 0; i < 2; i++) {
+	            opcionesEquipo[i] = equipo[i].getNombreClub();
+	        }
+	        eleccionEquipo = JOptionPane.showOptionDialog(null, "Elija una opcion", "Menu", 0, 0, null, opcionesEquipo, opcionesEquipo[0]);
+			
+			String[] operacion= {"Agregar Jugador", "Eliminar Jugador", "Ver cantidad de jugadores", "Obtener lista de jugadores", "Salir"};
+			eleccionOperacion = JOptionPane.showOptionDialog(null, "Elija una opcion", "Menu", 0, 0, null, operacion, operacion[0]);
+			
+			
+			switch (eleccionOperacion) {
+			case 0:
+				if(equipo[eleccionEquipo].AgregarJugador()) {
+					JOptionPane.showMessageDialog(null, "Se agrego exitosamente el jugador al equipo");
+					JOptionPane.showMessageDialog(null, equipo[eleccionEquipo].toString());
+				} else {		
+					JOptionPane.showMessageDialog(null, "No se agrego ningun jugador al equipo");
+				}
+				break;
+			case 1:
+				if(equipo[eleccionEquipo].EliminarJugador()) {
+					JOptionPane.showMessageDialog(null, "Se elimino exitosamente el jugador del equipo");
+					JOptionPane.showMessageDialog(null, equipo[eleccionEquipo].toString());
+				} else {		
+					JOptionPane.showMessageDialog(null, "No se elimino ningun jugador del equipo");
+				}
+				
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				
+				break;
+			case 4:
+				
+				break;
+				
+			default:
+				break;
+			}
+			
+			
+		} while (eleccionOperacion != 4);
 	}
 
 }
